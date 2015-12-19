5a7cf9b0297d08cecb1f4f843149ea436ffcb8f2
xiaoyaoyouzizai <xiaoyaoyouzizai@gmail.com>
2015-12-20 02:34:15 +0800

    Update test.scala

--- test.scala
+++ test.scala
@@ -1,15 +1,8 @@
-val file = sc.textFile("hdfs://192.168.1.140/data/9hong/orders.csv")
-// val fltr = file.filter(_.length > 0)
-// file.toArray.foreach(println)
-// val keys = fltr.map(_.split(",")).map(a => a(5))
-// keys.collect().foreach(println)
-// myLines_filtered.count()
-val stateSum = file.map(_.split(",")).map(key => (key(23).substring(1,14)+key(1)+key(11), (key(13).substring(1,key(13).length-1).toFloat*10000).toInt))
+val ordersRDD = sc.textFile("hdfs://192.168.1.140/data/9hong/orders.csv")
+val testUsersRDD = sc.textFile("hdfs://192.168.1.140/data/9hong/test_users.csv").flatMap(_.split(",")).map(k=>(k,null))
 
-// stateSum.toArray.foreach(println)
-// stateSum.collect().foreach(println)
-// val lastMap = stateSum.countByKey
-// val lastMap = stateSum.foldLeft(0)(_+_._2)
+val userOrdersRDD = file.map(_.split(",")).map(key => (key(6).substring(1,key(6).length-1), (key(23).substring(1,14) + "," + key(1).substring(1,key(1).length-1) + "," + key(11).substring(1,key(11).length-1), key(13).substring(1,key(13).length-1))))
+val sumRDD= userOrdersRDD.subtractByKey(testUsersRDD).map(x=>(x._2._1,x._2._2.toFloat)).reduceByKey((x,y)=>x+y)
 
-val lastMap = stateSum.groupBy(_._1).mapValues(_.map(_._2).sum)
-lastMap.saveAsTextFile("result2")
+
+sumRDD.saveAsTextFile("sum.csv")
